aef971a964c3bf3f4eb2087570e6f30f09f99e82
Timofspades <55791350+Timofspades@users.noreply.github.com>
2020-01-22 12:42:40 +0000

    First 1

---
+++ Assets/Scripts/BallForce.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallForce : MonoBehaviour
+{
+    private GameObject maincamera;
+    void Start()
+    {
+        maincamera = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+
+    private void OnMouseDown()
+    {
+        gameObject.GetComponent<Rigidbody>().AddForce(maincamera.transform.forward * 200.0f);
+    }
+}
---
+++ Assets/Scripts/CamraPositionScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamraPositionScript : MonoBehaviour
+{
+    public GameObject GolfBall;
+    public GameObject GolfCamra;
+    public GameObject CamPivot;
+
+    public Vector3 CamOffset;
+
+    float ZRot;
+    float YRot;
+
+    public float RotationSpeed;
+    void Start()
+    {
+        Vector3 currentEuler = CamPivot.transform.eulerAngles;
+        ZRot = currentEuler.z;
+        YRot = currentEuler.y;
+
+        GolfCamra.transform.localPosition = CamOffset;
+    }
+
+
+    void Update()
+    {
+        CamPivot.transform.position = GolfBall.transform.position;
+        GolfCamra.transform.localPosition = CamOffset;
+
+        if(Input.GetMouseButton(0))
+        {
+            float X = Input.GetAxis("Mouse X");
+            float Y = Input.GetAxis("Mouse Y");
+
+            ZRot -= Y * RotationSpeed * Time.deltaTime;
+            YRot += X * RotationSpeed * Time.deltaTime;
+
+            Vector3 currentEuler = CamPivot.transform.eulerAngles;
+
+            CamPivot.transform.rotation = Quaternion.Euler(ZRot, YRot, currentEuler.z);
+        }
+    }
+}
