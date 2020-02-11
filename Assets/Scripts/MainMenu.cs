4cbc94703e305cc7b96a1448738fd6b39b813575
Timofspades <55791350+Timofspades@users.noreply.github.com>
2020-02-11 20:00:43 +0000

    Works On Mobile

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene("Game Test Level");
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void ExitGame ()
+    {
+        Application.Quit();
+    }
+}
